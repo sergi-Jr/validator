4e65cbce0ed4a13b4cf35e401af638090cd03426
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-02-10 17:37:43 +0400

    Add javadoc

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -3,6 +3,9 @@ package hexlet.code.schemas;
 public abstract class BaseSchema<T> {
     private boolean isRequired;
 
+    /** Instructs the schema to check the passed object for the presence of that object.
+     * @return BaseSchema type
+     */
     protected BaseSchema<T> required() {
         isRequired = true;
         return this;
