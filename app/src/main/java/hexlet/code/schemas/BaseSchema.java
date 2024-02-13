0c71522e567c0b18e0d2c0032aa4da05ac760073
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-02-13 21:35:32 +0400

    Linter refactoring

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -8,10 +8,17 @@ import java.util.function.Predicate;
 public abstract class BaseSchema<T> {
     protected List<Predicate<T>> validations = new LinkedList<>();
 
+    /** Checks that all validations are pass
+     * @param object Strongly parametrized object
+     * @return true if all validations passed, false otherwise
+     */
     public boolean isValid(T object) {
         return validations.stream().allMatch(validation -> validation.test(object));
     }
 
+    /** Add non null validation
+     * @return This object type
+     */
     public BaseSchema<T> required() {
         validations.add(Objects::nonNull);
         return this;
